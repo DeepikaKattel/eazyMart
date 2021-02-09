64c3f3f5752309fe871772293ee9f48eccf4e48a
Deepika <deepika.kattel80@gmail.com>
2021-02-09 17:13:50 +0545

    Made carousel dynamic

--- resources/views/eazymart/index.blade.php
+++ resources/views/eazymart/index.blade.php
@@ -2,63 +2,27 @@
 @section('content')
 <!-- banner -->
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators-->
+
     <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1" class=""></li>
-        <li data-target="#myCarousel" data-slide-to="2" class=""></li>
-        <li data-target="#myCarousel" data-slide-to="3" class=""></li>
+    @foreach( $frontEnd as $front )
+    <!-- Indicators-->
+        <li data-target="#myCarousel" data-slide-to="{{ $loop->index }}" class="{{ $loop->first ? 'active' : '' }}"></li>
+    @endforeach
     </ol>
     <div class="carousel-inner" role="listbox">
-        <div class="item active">
+        @foreach( $frontEnd as $front )
+        <div class="item {{ $loop->first ? ' active' : '' }}" style="background: url('/storage/images/products/{{$front->image}}')">
             <div class="container">
                 <div class="carousel-caption">
-                    <h3>Big
-                        <span>Save</span>
+                    <h3>
+                        <span>{{$front->heading}}</span>
                     </h3>
-                    <p>Get flat
-                        <span>10%</span> Cashback</p>
+                    <p><span>{{$front->message}}</span></p>
                     <a class="button2" href="product.html">Shop Now </a>
                 </div>
             </div>
         </div>
-        <div class="item item2">
-            <div class="container">
-                <div class="carousel-caption">
-                    <h3>Healthy
-                        <span>Saving</span>
-                    </h3>
-                    <p>Get Upto
-                        <span>30%</span> Off</p>
-                    <a class="button2" href="product.html">Shop Now </a>
-                </div>
-            </div>
-        </div>
-        <div class="item item3">
-            <div class="container">
-                <div class="carousel-caption">
-                    <h3>Big
-                        <span>Deal</span>
-                    </h3>
-                    <p>Get Best Offer Upto
-                        <span>20%</span>
-                    </p>
-                    <a class="button2" href="product.html">Shop Now </a>
-                </div>
-            </div>
-        </div>
-        <div class="item item4">
-            <div class="container">
-                <div class="carousel-caption">
-                    <h3>Today
-                        <span>Discount</span>
-                    </h3>
-                    <p>Get Now
-                        <span>40%</span> Discount</p>
-                    <a class="button2" href="product.html">Shop Now </a>
-                </div>
-            </div>
-        </div>
+        @endforeach
     </div>
     <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
@@ -68,6 +32,7 @@
         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </a>
+
 </div>
 <!-- //banner -->
 
@@ -80,7 +45,7 @@
 					<i></i>
 					<i></i>
 					<i></i>
-				</span>
+            </span>
         </h3>
         <!-- //tittle heading -->
         <!-- product left -->
--- resources/views/layouts/eazyCommon.blade.php
+++ resources/views/layouts/eazyCommon.blade.php
@@ -199,6 +199,33 @@
 
     });
 </script>
+<script>
+    $(window).load(function () {
+        $("#frontCarousel").flexisel({
+            visibleItems: 3,
+            animationSpeed: 1000,
+            autoPlay: true,
+            autoPlaySpeed: 3000,
+            pauseOnHover: true,
+            enableResponsiveBreakpoints: true,
+            responsiveBreakpoints: {
+                portrait: {
+                    changePoint: 480,
+                    visibleItems: 1
+                },
+                landscape: {
+                    changePoint: 640,
+                    visibleItems: 2
+                },
+                tablet: {
+                    changePoint: 768,
+                    visibleItems: 2
+                }
+            }
+        });
+
+    });
+</script>
 <!-- //flexisel (for special offers) -->
 
 <!-- password-script -->
